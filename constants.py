bf3e30510c3410f9b40a3b725b31f4d6588dffb6
Josh <jcjohnson77@gmail.com>
2023-11-09 21:00:44 -0500

    Try using a full curl

--- constants.py
+++ constants.py
@@ -3,5 +3,21 @@ DEBUG = False
 CURL_TIMEOUT = 10
 SLEEP_TIME = 5
 
-NP_CURL = "curl -k -X GET -H 'Content-type: application/json' 'https://jjdev.io/npbot/data.php?game_id=%s&v=%s'"
+NP_CURL = """
+curl -k 'https://jjdev.io/npbot/data.php?game_id=%s&v=%s' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-US,en;q=0.9' \
+  -H 'Cache-Control: max-age=0' \
+  -H 'Connection: keep-alive' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: none' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36' \
+  -H 'sec-ch-ua: "Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  --compressed
+"""
 WEBHOOK_CURL = "curl -X POST -H 'Content-type: application/json' --data '%s' %s"
